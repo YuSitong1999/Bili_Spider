8bcfc250a9771d833e45faadf8c6256c18cb97e8
于斯同YuSitong <YuSitong1999@foxmail.com>
2019-12-03 14:34:56 +0800

    使用ffmpeg合并下载的音视频m4s文件

--- Spider.py
+++ Spider.py
@@ -18,8 +18,10 @@ start_time = datetime.datetime.now()
 
 def getBaseUrl():
     # url = input()
-    url = 'https://www.bilibili.com/video/av7923312/'
-    # url = 'https://www.bilibili.com/video/av75993929'
+    # url = 'https://www.bilibili.com/video/av7923312/'
+    # url = 'https://www.bilibili.com/video/av6477644'
+
+    url = 'https://www.bilibili.com/video/av75993929'
 
     # url_host = url.match(r'(?<=https://)(.*?)(?=/video)')
     url_host = re.search(r'https://(.*?)/video', url).group(1)
@@ -64,6 +66,13 @@ def getBaseHtml(base_url_host, base_url):
     return html
 
 
+def checkM4S(base_html):
+    result = re.findall(r'\.m4s\?', base_html)
+    print(result)
+    return result != []
+        # (r'\.(.+?)\?', base_html).group(1)
+
+
 def getInfo(html):
     video_url = re.search(r'"url":"(.*?)","backup_url"', html).group(1)  # 这个是URL
     print('video_url')
@@ -84,7 +93,31 @@ def getInfo(html):
     return host, video_url, title, format
 
 
-def download(base_url, host, video_url, title, format):
+def getM4SInfo(html):
+    video_url = re.search(r'"video":\[\{"id":\d*,"baseUrl":"(.*?)","base_url"', html).group(1)  # 这个是URL
+    print('video_url')
+    print(video_url)
+
+    audio_url = re.search(r'"audio":\[\{"id":\d*,"baseUrl":"(.*?)","base_url"', html).group(1)  # 这个是URL
+    print('audio_url')
+    print(audio_url)
+
+    host = re.search(r'http://(.*?)/upgcxcode', video_url).group(1)
+    print('host')
+    print(host)
+
+    title = re.search(r'<title data-vue-meta="true">(.*?)_哔哩哔哩', html).group(1)
+    print('title')
+    print(title)
+
+    format = re.search(r'\.(.+?)\?', video_url).group(1).split('.')[-1]
+    print('format')
+    print(format)
+
+    return host, video_url, audio_url, title, format
+
+
+def download(base_url, host, video_url, save_path):
     # GET /upgcxcode/15/90/13009015/13009015-1-64.flv?e=ig8euxZM2rNcNbhHhbUVhoMznWNBhwdEto8g5X10ugNcXBlqNxHxNEVE5XREto8KqJZHUa6m5J0SqE85tZvEuENvNo8g2ENvNo8i8o859r1qXg8xNEVE5XREto8GuFGv2U7SuxI72X6fTr859r1qXg8gNEVE5XREto8z5JZC2X2gkX5L5F1eTX1jkXlsTXHeux_f2o859IB_&uipk=5&nbs=1&deadline=1575298307&gen=playurl&os=ks3u&oi=3670383011&trid=1e68e83c0d404e039b754dfa1d872c94u&platform=pc&upsig=a2b51986300359e9463f567a03c920c3&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&mid=95451953 HTTP/1.1
     # Host: upos-hz-mirrorks3u.acgvideo.com
     # Connection: keep-alive
@@ -108,8 +141,8 @@ def download(base_url, host, video_url, title, format):
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-CN,zh;q=0.9',
     }
-    if not os.path.exists(title + '.' + format):
-        with open(title + '.' + format, 'wb') as f:
+    if not os.path.exists(save_path):
+        with open(save_path, 'wb') as f:
             print("-------------------------STart LINE-------------------------")
             localtime = time.strftime("%Y-%m-%d %H:%M:%S")
             print(localtime + "开始下载")
@@ -121,12 +154,29 @@ def download(base_url, host, video_url, title, format):
     else:
         print("视频  存在于本地，跳过下载")
 
+
 # 主函数
 def main():
     base_url_host, base_url = getBaseUrl()
     base_html = getBaseHtml(base_url_host, base_url)
-    host, video_url, title, format = getInfo(base_html)
-    download(base_url, host, video_url, title, format)
+    if checkM4S(base_html):
+        host, video_url, audio_url, title, format = getM4SInfo(base_html)
+        abspath = '\\'.join(os.path.abspath(__file__).split('\\')[0:-1]) + '\\'
+        # title = '2019'
+        video_path = abspath + title + 'video.' + format
+        audio_path = abspath + title + 'audio.' + format
+        merge_path = abspath + title + '.mp4'
+        print(os.path.abspath(__file__))
+        download(base_url, host, video_url, video_path)
+        download(base_url, host, audio_url, audio_path)
+        command = r'FFmpeg -i "' + video_path + r'" -i "' + audio_path + r'" -codec copy "' + merge_path + r'"'
+        print(command)
+        os.system(command)
+    else:
+        host, video_url, title, format = getInfo(base_html)
+        download(base_url, host, video_url, title + '.' + format)
+
+
 
     return
 
